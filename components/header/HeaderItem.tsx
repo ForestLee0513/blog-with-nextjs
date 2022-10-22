ce50379e30ac47eee976522381c087f32bf2efe8
ForestLee0513 <woolimlee.dev@gmail.com>
2022-10-22 18:06:40 +0900

    feature: Add header list component

---
+++ components/header/HeaderItem.tsx
@@ -0,0 +1,33 @@
+import { HTMLAttributes } from "react";
+
+const HeaderItem = ({
+  children,
+  className = "",
+  ...rest
+}: HTMLAttributes<HTMLLIElement>) => {
+  return (
+    <li
+      className={`flex justify-end m-0 px-4 first:pl-0 last:pr-0 ${className}`}
+      {...rest}
+    >
+      {children}
+    </li>
+  );
+};
+
+const HeaderItemFull = ({
+  children,
+  className = "",
+  ...rest
+}: HTMLAttributes<HTMLLIElement>) => {
+  return (
+    <li
+      className={`flex justify-end m-0 px-4 flex-1 first:pl-0 last:pr-0 ${className}`}
+      {...rest}
+    >
+      {children}
+    </li>
+  );
+};
+
+export { HeaderItem, HeaderItemFull };
---
+++ components/header/HeaderList.tsx
@@ -0,0 +1,18 @@
+import { HTMLAttributes } from "react";
+
+const HeaderList = ({
+  children,
+  className = "",
+  ...rest
+}: HTMLAttributes<HTMLUListElement>) => {
+  return (
+    <ul
+      className={`flex flex-1 flex-wrap items-center list-none p-0 w-full ${className}`}
+      {...rest}
+    >
+      {children}
+    </ul>
+  );
+};
+
+export default HeaderList;
