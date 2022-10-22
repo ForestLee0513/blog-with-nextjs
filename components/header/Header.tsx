a21571af7c17dba482fa95d1398b82d0de466277
ForestLee0513 <woolimlee.dev@gmail.com>
2022-10-22 18:06:18 +0900

    refactor: Pass children and rest props to header

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,22 +1,14 @@
-import Link from "next/link";
 import { HTMLAttributes } from "react";
 import Container from "../Container";
 
 const Header = ({
-  className = "",
+  className,
   children,
   ...rest
 }: HTMLAttributes<HTMLElement>) => {
-  const userName = process.env.NEXT_PUBLIC_USERNAME || "Tailwind blog starter";
-
   return (
     <header className={`py-4 border-b ${className}`} {...rest}>
-      <Container>
-        <Link href="/">
-          <h3 className="cursor-pointer">{userName}</h3>
-        </Link>
-        {children}
-      </Container>
+      <Container className="flex items-center">{children}</Container>
     </header>
   );
 };
