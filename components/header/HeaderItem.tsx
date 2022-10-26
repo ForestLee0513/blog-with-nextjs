5d9a20428eafdabb9562bdb718ad7e936ebaa3b6
ForestLee0513 <woolimlee.dev@gmail.com>
2022-10-26 13:15:12 +0900

    fix: Remove padding at first child in HeaderItem

--- components/header/HeaderItem.tsx
+++ components/header/HeaderItem.tsx
@@ -6,7 +6,10 @@ const HeaderItem = ({
   ...rest
 }: HTMLAttributes<HTMLLIElement>) => {
   return (
-    <li className={`flex justify-end m-0 last:pr-0 ${className}`} {...rest}>
+    <li
+      className={`flex justify-end m-0 first:pl-0 last:pr-0 ${className}`}
+      {...rest}
+    >
       {children}
     </li>
   );
@@ -19,7 +22,7 @@ const HeaderItemFull = ({
 }: HTMLAttributes<HTMLLIElement>) => {
   return (
     <li
-      className={`flex justify-end m-0 flex-1 last:pr-0 ${className}`}
+      className={`flex justify-end m-0 flex-1 first:pl-0 last:pr-0 ${className}`}
       {...rest}
     >
       {children}
