2fbb9fcb9b3395d959a1bbd7ccf134d78337fec9
ForestLee0513 <woolimlee.dev@gmail.com>
2022-10-31 19:08:56 +0900

    fix: Change site url to demo page

--- bio.ts
+++ bio.ts
@@ -1,7 +1,7 @@
 const bio = {
   username: "Username",
   description: "hello world",
-  url: "https://forestlee-nextjs-blog-test.netlify.app",
+  url: "https://forestlee-nextjs-blog-demo.netlify.app",
   link: {
     Github: "https://github.com/username",
   },
