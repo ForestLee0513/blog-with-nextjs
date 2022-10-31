91a441210ab68d2d9a0c18234026fd8a30f3b244
ForestLee0513 <woolimlee.dev@gmail.com>
2022-10-31 16:31:11 +0900

    fix: Change origin url

--- bio.ts
+++ bio.ts
@@ -1,7 +1,7 @@
 const bio = {
   username: "Username",
   description: "hello world",
-  url: "https://forestlee0513.github.io",
+  url: "https://forestlee-nextjs-blog-test.netlify.app",
   link: {
     Github: "https://github.com/username",
   },
